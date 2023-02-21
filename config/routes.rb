7dd5254145edbdb02dd2cca7d705b1b373956305
Nicolo Adamo <nicolo.adamo@gmail.com>
2023-02-21 19:04:40 +0100

    to do: make it look nicer

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # list all
+  get "tasks", to: "tasks#index"
+  # create
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  # read one
+  get "tasks/:id", to:"tasks#show", as: :task
+  # update one
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  # delete one
+  delete "tasks/:id", to:"tasks#destroy"
+
 end
